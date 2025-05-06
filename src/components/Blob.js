f9675e73cbc02da0d60fa8d527567f694ac3872f
danaeswart <241022@virtualwindow.co.za>
2025-05-06 12:57:57 +0200

    Home page developed
     - backgrorund and info added -- a few hover effects and background effects --

---
+++ src/components/Blob.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../components/Blob.css';
+
+const Blob = () => {
+  return (
+    <div className="blob-container">
+      <div className="blob blob1"></div>
+      <div className="blob blob2"></div>
+      <div className="blob blob3"></div>
+      <div className='blob blob4'></div>
+    </div>
+  );
+};
+
+export default Blob;
